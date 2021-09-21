51a0b2e2e946b659ad7ff66ff3c91b7dfee437a9
Soroush Trb <tspersian@gmail.com>
2021-09-21 02:06:42 -0700

    a

--- SetareFolad/My Project/Settings.Designer.vb
+++ SetareFolad/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.SetareFolad.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Mim.My.MySettings
             Get
-                Return Global.SetareFolad.My.MySettings.Default
+                Return Global.Mim.My.MySettings.Default
             End Get
         End Property
     End Module
